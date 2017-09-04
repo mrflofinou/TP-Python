cb3b5e3721afb2a5c15cf3bd9843b50eb69c80c8
Florian <florian.lescuyer@gmail.com>
2017-09-04 11:02:52 +0200

    modifications in pendu.py and fonctions.py

--- pendu/fonctions.py
+++ pendu/fonctions.py
@@ -12,36 +12,17 @@ def mot_hasard(nbr_hasard, liste_mots):
     mot_hasard = list(mot_hasard)         # transforme la chaine de caractère en liste afin de pouvoir faire des comparaison avec la liste mot_trouve
     return mot_hasard
 
-def recherche_mot(mot_hasard, compteur):
+def choix_lettre():
     """
-    Cette fonction sert à chercher le mot.
-    On créé une liste qui sera le mot trouvé. Cette liste sera comparé au mot au hasard
-    Cette liste sera aussi remplie des lettre trouvées au fur et à mesure de la partie
-    La fonction renvoie le mot trouvé sous forme de liste
+    Cette fonction permet de récupérer la lettre choisie par le joueur
     """
-
-    mot_trouve = []                       # On initialise la variable mot_trouve comme une liste
-    entree = str()
-
-    for lettre in mot_hasard:
-        mot_trouve.append("*")            # On rempli la liste mot_trouve par des étoiles pour l'affichage
-    affichage = "".join(mot_trouve)       # Afin que l'affichage soit propre on affiche une chaine de caractère
-    print("Le mot recherché contient {} lettres:\n{}".format(len(mot_hasard), affichage.center(30)))
-
-    while mot_trouve != mot_hasard:           # Tant que les deux liste ne sont pas égale c'est qu'on a pas trouvé le mot, alors on continue
-        entree = input("\nTapez une lettre: ")
-        entree = entree.lower()
-        for i, lettre in enumerate(mot_hasard):
-            if entree == lettre:
-                mot_trouve[i] = mot_hasard[i]   # Si la lettre entrée correspond à une lettre du mot alors elle est enregistrée dans le tableau mot_trouve
-        affichage = "".join(mot_trouve)         # Cette affichage permet de montrer uniquement les lettres trouvées dans le mot
-        print ("\n",affichage.center(30))
-        compteur-= 1                            # Le compteur permet permet de compter le nombre de tentatives restantes
-        if compteur == 0:                       # Si le compteur est à 0 c'est qu'on épuisé toutes nos tetative
-            break                               # Du coup on casse la boucle
-        else:                                   # Sinon on affiche le nombre de tentatives restantes
-            print("\nil te reste encore {} essais".format(compteur))
-    return mot_trouve
+    entree = input("\nTapez une lettre: ")
+    entree = entree.lower()
+    if len(entree)>1 or not entree.isalpha():
+        print("Vous n'avez pas saisi une lettre valide.")
+        return choix_lettre()
+    else:
+        return entree
 
 def affichage_resultat(mot_hasard, mot_trouve):
     """
@@ -49,7 +30,7 @@ def affichage_resultat(mot_hasard, mot_trouve):
     Elle compare si le mot renvoyé par la fonction "recherche_mot" correspond au mot choisi au hasard
     renvoyé par la fonction "mot_hasard" et affiche un texte en fonction des cas
     """
-    
+
     if mot_trouve == mot_hasard:
         print("\nBravo ! tu as trouvé le mot '{}'".format("".join(mot_hasard)))
     else:
--- pendu/pendu.py
+++ pendu/pendu.py
@@ -19,7 +19,31 @@ import fonctions
 liste_mots = donnees.liste_mot
 compteur = donnees.nombre_chances                           # nombre de tentatives autorisées
 nbr_hasard = random.randrange(len(donnees.liste_mot))
-
 mot_hasard = fonctions.mot_hasard(nbr_hasard, liste_mots)
-mot_trouve = fonctions.recherche_mot(mot_hasard, compteur)
+
+mot_trouve = []                       # On initialise la variable mot_trouve comme une liste
+entree = str()                        # On initialise la variable entree comme une chaine de caractères.
+for lettre in mot_hasard:
+    mot_trouve.append("*")            # On rempli la liste mot_trouve avec des étoiles pour l'affichage
+affichage = "".join(mot_trouve)       # Afin que l'affichage soit propre on affiche une chaine de caractère
+print("Le mot recherché contient {} lettres:\n{}".format(len(mot_hasard), affichage.center(30)))
+
+while mot_trouve != mot_hasard:           # Tant que les deux liste ne sont pas égale c'est qu'on a pas trouvé le mot, alors on continue
+    entree = fonctions.choix_lettre()
+    for i, lettre in enumerate(mot_hasard):
+        if entree == lettre:
+            mot_trouve[i] = mot_hasard[i]   # Si la lettre entrée correspond à une lettre du mot alors elle est enregistrée dans le tableau mot_trouve
+    affichage = "".join(mot_trouve)         # Cette affichage permet de montrer uniquement les lettres trouvées dans le mot
+    print ("\n",affichage.center(30))
+
+    if entree in mot_hasard:                # Si la lettre est dans le mot alors le compteur ne change pas
+        compteur = compteur
+    else:
+        compteur-= 1                        # Le compteur permet permet de compter le nombre de tentatives restantes
+
+    if compteur == 0:                       # Si le compteur est à 0 c'est qu'on épuisé toutes nos tetative
+        break                               # Du coup on casse la boucle
+    else:                                   # Sinon on affiche le nombre de tentatives restantes
+        print("\nil te reste encore {} essais".format(compteur))
+
 fonctions.affichage_resultat(mot_hasard, mot_trouve)
